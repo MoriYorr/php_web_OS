2a82b868d437f13ae34fc40fdf62c0094d8fab30
MoriYorr <yourmori1@gmail.com>
2025-11-11 04:38:39 +0000

    feat: add basics_detailed.php

---
+++ basic_detailed.php
@@ -0,0 +1,32 @@
+function classifyAge(int  $age): string {
+	if ($age > 17){
+		return "Взрослый";
+	}
+	elseif ($age > 11){
+                return "Подросток";
+        }
+	else {
+		return "Ребенок";
+	}
+}
+
+echo classifyAge(8)
+echo classifyAge(15)
+echo classifyAge(25)
+
+$cities = ["Москва", "Санкт-Петербург", "Казань", "Екатеринбург", "Чита"]
+foreach ($city in $cities){
+	echo $city . "\n";
+}
+
+for ($i = 0; $i < 100, $i++){
+	if($i % 3 == 0 && $i % 5 ==0){
+		echo "FizzBuzz";
+	}
+	elseif ($i % 3 == 0){
+		echo "Fizz";
+	}
+	elseif ($i % 5 == 0){
+		echo "Buzz";
+	}
+}
