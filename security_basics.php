8d21e1542aef59197e4e9de816c74f2b439de1f7
MoriYorr <190569440+MoriYorr@users.noreply.github.com>
2025-11-27 11:46:06 +0000

    lab 10 start

--- security_basics.php
+++ security_basics.php
@@ -1,3 +1,12 @@
 <?php
-echo "again";
+function validateEmail(string $email): ?string{
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        return $email;
+    }
+    return null;
+}
+
+function validateName(string $name): ?string{
+    
+}
 ?>
