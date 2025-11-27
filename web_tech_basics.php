9618ea01a6b3d5b9de2dcc0f0490bca773ff00a5
MoriYorr <190569440+MoriYorr@users.noreply.github.com>
2025-11-27 07:28:01 +0000

    task1

--- web_tech_basics.php
+++ web_tech_basics.php
@@ -1,3 +1,24 @@
 <?php
+echo "Hello";
+function dumpRequestInfo(): void{
+    echo '<pre>';
+    echo 'Метод запроса: ' . htmlspecialchars($_SERVER['REQUEST_METHOD']) . "\n";
+    echo 'URI: ' . htmlspecialchars($_SERVER['REQUEST_URI']) . "\n";
+    if (!empty($_GET)) {
+    echo "GET-параметры:\n";
+    echo htmlspecialchars(print_r($_GET, true)) . "\n";
+}
 
+if (!empty($_POST)) {
+    echo "POST-параметры:\n";
+    echo htmlspecialchars(print_r($_POST, true)) . "\n";
+}
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    echo 'Браузер: ' . htmlspecialchars($_SERVER['HTTP_USER_AGENT']) . "\n";
+}
+    echo '</pre>';
+}
+
+dumpRequestInfo();
+echo "Hello";
 ?>
